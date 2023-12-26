7a3b516426c4d35d0c4844b69d227443d1f548fc
Onimae <manufelipearias@gmail.com>
2023-12-26 20:53:27 +1100

    added cors package and set up cors whitelist of url for request

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const routerAPIRequest = require('./routes/index.js');
@@ -10,6 +11,18 @@ app.get('/api-key', (req, res) => {
 routerAPIRequest(app);
 
 const PORT = process.env.PORT || 3000;
+
+const whiteList = ['https://oni-movies-frontend.vercel.app/'];
+const options = {
+  origin: (origin, callback) => {
+    if (whiteList.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not Allowed by CORS'));
+    }
+  },
+};
+app.use(cors(options));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
