e1f64cb783a79b1b8672144a7e4cf4fe517fe83d
Onimae <manufelipearias@gmail.com>
2024-01-01 22:29:26 +1100

    gettin movies by id and storing on DB

--- api/controllers/movieById.controller.js
+++ api/controllers/movieById.controller.js
@@ -1,14 +1,60 @@
 const api = require('./APIRequest.controller');
+const prisma = require('../../prisma/prisma');
+
+async function checkDatabaseForMovieById(id) {
+  const ifInfoOnDb = await prisma.movieDB_API_responses.findFirst({
+    where: {
+      query: id,
+      contollerType: 'movieById',
+      createdAt: {
+        gte: new Date(new Date() - 1000 * 60 * 60 * 24),
+      },
+    },
+  });
+
+  if (!ifInfoOnDb) {
+    return null;
+  }
+
+  return ifInfoOnDb.data;
+}
+
+async function fetchAndStoreMovieById(id) {
+  const { data } = await api.get(`movie/${id}`);
+  const movie = data;
+  await prisma.movieDB_API_responses.create({
+    data: {
+      data: movie,
+      contollerType: 'movieById',
+      query: id,
+    },
+  });
+  return movie;
+}
 
 async function getdMoviesById(id) {
   try {
-    const { data } = await api.get(`movie/${id}`);
+    const movie = await checkDatabaseForMovieById(id);
+    if (movie) {
+      return movie;
+    }
 
-    return data;
+    return await fetchAndStoreMovieById(id);
   } catch (error) {
-    console.log(error.message);
     throw new Error(error.message);
   }
 }
+
+// async function getdMoviesById(id) {
+
+//   try {
+//     const { data } = await api.get(`movie/${id}`);
+
+//     return data;
+//   } catch (error) {
+//     console.log(error.message);
+//     throw new Error(error.message);
+//   }
+// }
 
 module.exports = getdMoviesById;
--- api/controllers/moviesByCategory.controller.js
+++ api/controllers/moviesByCategory.controller.js
@@ -49,21 +49,4 @@ async function getMoviesByCategory(id) {
   }
 }
 
-// async function getMoviesByCategory(id) {
-//   try {
-//     const { data } = await api.get('discover/movie', {
-//       params: {
-//         with_genres: id,
-//       },
-//     });
-
-//     const movies = data.results;
-
-//     return movies;
-//   } catch (error) {
-//     console.log(error.message);
-//     throw new Error(error.message);
-//   }
-// }
-
 module.exports = getMoviesByCategory;
