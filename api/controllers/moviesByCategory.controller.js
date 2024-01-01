fe3e4768169b0e0acf5cfb39ed152f053bc248d4
Onimae <manufelipearias@gmail.com>
2024-01-01 22:27:02 +1100

    getting related movies by category and storing results on db

--- api/controllers/moviesByCategory.controller.js
+++ api/controllers/moviesByCategory.controller.js
@@ -1,20 +1,69 @@
 const api = require('./APIRequest.controller');
+const prisma = require('../../prisma/prisma');
+
+async function checkDatabaseForMoviesByCategory(id) {
+  const ifInfoOnDb = await prisma.movieDB_API_responses.findFirst({
+    where: {
+      query: id,
+      contollerType: 'MovieByCategory',
+      createdAt: {
+        gte: new Date(new Date() - 1000 * 60 * 60 * 24),
+      },
+    },
+  });
+
+  if (!ifInfoOnDb) {
+    return null;
+  }
+
+  return ifInfoOnDb.data;
+}
+
+async function fetchAndStoreMoviesByCategory(id) {
+  const { data } = await api.get('discover/movie', {
+    params: {
+      with_genres: id,
+    },
+  });
+  const movies = data.results;
+  await prisma.movieDB_API_responses.create({
+    data: {
+      data: movies,
+      contollerType: 'MovieByCategory',
+      query: id,
+    },
+  });
+  return movies;
+}
 
 async function getMoviesByCategory(id) {
   try {
-    const { data } = await api.get('discover/movie', {
-      params: {
-        with_genres: id,
-      },
-    });
+    const movies = await checkDatabaseForMoviesByCategory(id);
+    if (movies) {
+      return movies;
+    }
 
-    const movies = data.results;
-
-    return movies;
+    return await fetchAndStoreMoviesByCategory(id);
   } catch (error) {
-    console.log(error.message);
     throw new Error(error.message);
   }
 }
+
+// async function getMoviesByCategory(id) {
+//   try {
+//     const { data } = await api.get('discover/movie', {
+//       params: {
+//         with_genres: id,
+//       },
+//     });
+
+//     const movies = data.results;
+
+//     return movies;
+//   } catch (error) {
+//     console.log(error.message);
+//     throw new Error(error.message);
+//   }
+// }
 
 module.exports = getMoviesByCategory;
